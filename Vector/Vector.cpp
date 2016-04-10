d210d45bc579d07d50ab069c95c9090e4b5a79fc
Eustacia Rajman <eustacia_rajman@telkomsa.net>
2016-04-10 17:01:06 +0200

    Fix random bug

--- Vector/Vector.cpp
+++ Vector/Vector.cpp
@@ -16,7 +16,7 @@ int main()
 	srand(time(NULL)); // seed randomizer
 	for (i = 0; i < 20; i++) 
 	{
-		randomNo = rand() * 99 + 1; // randomize numbers across an arbitrary range
+		randomNo = rand() % 99 + 1; // randomize numbers across an arbitrary range
 		vect.push_back(randomNo);
 	}
 	
