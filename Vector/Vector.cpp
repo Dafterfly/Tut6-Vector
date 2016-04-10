3b80e8d0c398ad722d99d5a2fc45b6467d44813c
Eustacia Rajman <eustacia_rajman@telkomsa.net>
2016-04-10 16:41:49 +0200

    Create a vector

--- Vector/Vector.cpp
+++ Vector/Vector.cpp
@@ -1,5 +1,14 @@
 #include <iostream> 
 #include <algorithm> 
 #include <vector> 
+#include <ctime>
 using namespace std;
 
+int main() {
+	vector <int> vect; // create an arbitrary sized vector
+
+	int i;
+
+	
+	return 0;
+}
